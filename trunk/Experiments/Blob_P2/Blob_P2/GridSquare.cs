caa58acd09090b75e19e11434bb79de04001769e
ben.kenny <ben.kenny@96dc84fd-2751-0410-8b94-2d7ca8d38da3>
2008-10-10 09:21:41 +0000

    Added missing file :(

---
+++ trunk/Experiments/Blob_P2/Blob_P2/GridSquare.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Blob_P2
+{
+    class GridSquare
+    {
+        public List<BlobParticle> particles;
+
+        public GridSquare(Vector2 position, Vector2 dimension)
+        {
+
+        }
+    }
+}
