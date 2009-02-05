cccac957b59742cd3277ad2bcd6ab1f3196b7c9c
Ben <ben.kenny@gmail.com>
2009-02-05 00:02:43 +0000

    whoops

---
+++ Parasite/Parasite/GUI/GUIOpenFileDialog.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace Parasite
+{
+    /// <summary>
+    /// A panel is a container of GUI components. Acts sort of like a moveable window.
+    /// </summary>
+    class GUIOpenFileDialog : GUIPanel
+    {
+        public GUIOpenFileDialog(Game game) : base(game, new Vector2(100, 100), new Vector2(600, 100), "ofdialog", "Open File...")
+        {         
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+            GUILabel label = new GUILabel(Game, new Vector2(10, 10), "lblOpen", "Filename:");
+            label.Initialize();
+            AddComponent(label);
+            GUIEditBox edit = new GUIEditBox(Game, new Vector2(100, 10), "edtOpen", 300, "");
+            edit.Initialize();
+            AddComponent(edit);
+            GUIButton ok = new GUIButton(Game, new Vector2(480, 50), "okbutton", "Ok");
+            ok.Initialize();
+            ok.OnMouseClick += new GUIButton.MouseClickHandler(OkMouseClick);
+            AddComponent(ok);
+            GUIButton cancel = new GUIButton(Game, new Vector2(520, 50), "cancelbutton", "Cancel");
+            cancel.Initialize();
+            cancel.OnMouseClick += new GUIButton.MouseClickHandler(CancelMouseClick);
+            AddComponent(cancel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        void CancelMouseClick(GUIComponent sender, OnMouseClickEventArgs args)
+        {
+
+            ((GUIManager)Game.Services.GetService(typeof(IGUIManager))).RemoveComponent(Name);
+        }
+
+        void OkMouseClick(GUIComponent sender, OnMouseClickEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+
+   }
+}
