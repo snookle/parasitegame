ed4455805f6f1acba188fffc5642f4b2dffc8c06
ben.kenny@gmail.com <ben.kenny@gmail.com@96dc84fd-2751-0410-8b94-2d7ca8d38da3>
2009-04-29 11:20:39 +0000

    Added physics control panel.

---
+++ trunk/Parasite/Parasite/GUI/GUIPhysicsControls.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace Parasite
+{
+    /// <summary>
+    /// A panel is a container of GUI components. Acts sort of like a moveable window.
+    /// </summary>
+    class GUIPhysicsControls : GUIPanel
+    {
+        // Texture Information
+        //string Tex_name;
+        //float Tex_layer;
+        //LevelArt Art;
+
+        bool useTexture = false;
+
+        // Information
+        Vector2 Dimensions = new Vector2(10, 10);
+
+        public GUIPhysicsControls(Game game)
+            : base(game, new Vector2(200, 10), new Vector2(125, 135), "physcontroller", "Physics Controls")
+        {
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            this.minimised = false;
+            base.Initialize();
+
+            GUIButton btn = new GUIButton(Game, new Vector2(10, 10), "rewind", "<<");
+            btn.Initialize();
+            AddComponent(btn);
+
+            btn = new GUIButton(Game, new Vector2(40, 10), "pause", "| |");
+            btn.Initialize();
+            AddComponent(btn);
+
+            btn = new GUIButton(Game, new Vector2(65, 10), "play", " > ");
+            btn.Initialize();
+            AddComponent(btn);
+
+            GUICheckbox cb = new GUICheckbox(Game, new Vector2(10, 50), "debugdraw", "Debug Draw");
+            cb.Initialize();
+            AddComponent(cb);
+
+            GUILabel lbl = new GUILabel(Game, new Vector2(10, 80), "status", "Status: Paused");
+            lbl.Initialize();
+            AddComponent(lbl);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        void CancelMouseClick(GUIComponent sender, OnMouseClickEventArgs args)
+        {
+
+            ((GUIManager)Game.Services.GetService(typeof(IGUIManager))).RemoveComponent(Name);
+        }
+    }
+}
--- trunk/Parasite/Parasite/PhysicsManagement/PhysicsManager.cs
+++ trunk/Parasite/Parasite/PhysicsManagement/PhysicsManager.cs
@@ -23,9 +23,37 @@ namespace Parasite
         InputHandler input;
         DeveloperConsole console;
         Camera camera;
+        GUIManager guimanager;
+
+        public bool Paused
+        {
+            get { return paused; }
+            set
+            {
+                paused = value;
+                if (paused)
+                {
+                    //pause it
+                    if (Simulator != null)
+                        Simulator.Enabled = false;
+                    
+                    if (statusLabel != null)
+                        statusLabel.Text = "Status: Paused";
+                }
+                else
+                {
+                    //unpause it
+                    if (Simulator != null)
+                        Simulator.Enabled = true;
+                    
+                    if (statusLabel != null)
+                        statusLabel.Text = "Status: Running";
+                }
+            }
+        }
 
         bool debugDraw = true;              // Debug draw should be on by default in edit mode
-        private bool simulate = true;       // Simulate on by default
+        private bool paused;
 
         float timeStep = 5.0f / 60f;
 
@@ -37,6 +65,9 @@ namespace Parasite
         public PhysicsSimulator Simulator;
         FarseerGames.FarseerPhysics.PhysicsSimulatorView debugView;
         SpriteBatch debugBatch;
+        GUIPhysicsControls controlPanel;
+        GUILabel statusLabel;
+        GUICheckbox cbDebugDraw;
 
 
         public PhysicsManager(Game game)
@@ -74,7 +105,7 @@ namespace Parasite
             {
                 if (string.IsNullOrEmpty(argument))
                 {
-                    console.Write("phys_pause is " + (simulate ? "1" : "0"));
+                    console.Write("phys_pause is " + (paused ? "1" : "0"));
                 }
                 else
                 {
@@ -82,9 +113,9 @@ namespace Parasite
                     {
                         int arg = Convert.ToInt32(argument);
                         if (arg == 0)
-                            simulate = false;
+                            Paused = false;
                         else
-                            simulate = true;
+                            Paused = true;
                     }
                     catch (Exception)
                     {
@@ -112,12 +143,45 @@ namespace Parasite
             console = (DeveloperConsole)Game.Services.GetService(typeof(IDeveloperConsole));
             console.MessageHandler += new DeveloperConsole.DeveloperConsoleMessageHandler(ConsoleMessageHandler);
             camera = (Camera)Game.Services.GetService(typeof(ICamera));
+            guimanager = (GUIManager)Game.Services.GetService(typeof(IGUIManager));
             
             // Define the World
             Simulator = new PhysicsSimulator(new Vector2(0, 8));
             debugView = new PhysicsSimulatorView(Simulator);
 
             base.Initialize();
+
+            controlPanel = new GUIPhysicsControls(Game);
+            controlPanel.Initialize();
+            guimanager.AddComponent(controlPanel);
+            GUIButton btn = (GUIButton)controlPanel.getComponent("pause");
+            btn.OnMouseClick += new GUIButton.MouseClickHandler(PauseButtonMouseClick);
+
+            btn = (GUIButton)controlPanel.getComponent("play");
+            btn.OnMouseClick += new GUIButton.MouseClickHandler(PlayButtonMouseClick);
+
+            statusLabel = (GUILabel)controlPanel.getComponent("status");
+            //start off paused.
+            Paused = true;
+
+            GUICheckbox cb = (GUICheckbox)controlPanel.getComponent("debugdraw");
+            cb.CheckStateChange += new GUICheckbox.CheckChangeEventHandler(DebugDrawCheckStateChanged);
+            cb.Checked = debugDraw;
+        }
+
+        void DebugDrawCheckStateChanged(GUIComponent sender, CheckStateEventArgs args)
+        {
+            debugDraw = args.Checked;
+        }
+
+        void PlayButtonMouseClick(GUIComponent sender, OnMouseClickEventArgs args)
+        {
+            Paused = false;
+        }
+
+        void PauseButtonMouseClick(GUIComponent sender, OnMouseClickEventArgs args)
+        {
+            Paused = true;
         }
 
         /// <summary>
@@ -126,9 +190,7 @@ namespace Parasite
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            if (simulate)
-                Simulator.Update(gameTime.ElapsedGameTime.Milliseconds * 0.005f);
-
+            Simulator.Update(gameTime.ElapsedGameTime.Milliseconds * 0.005f);
             base.Update(gameTime);
         }
 
